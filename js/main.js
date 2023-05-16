823475be63a094de00e7d59957d18733060eb6b5
Anton Solokha <anton.saloha@powercode.us>
2023-05-16 18:15:54 +0300

    fix deep object, formatting

--- js/main.js
+++ js/main.js
@@ -5,67 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const output = document.querySelector('#output')
     const button = document.querySelector('#submit')
 
-    // parse function
-
-    // example:
-    // input:
-    // position: 'relative',
-    //   textAlign: 'end',
-    //   padding: '8px 0',
-    //   margin: '0 16px',
-    //   '& span': {
-    //     background: '#fff',
-    //     position: 'relative',
-    //     zIndex: 1,
-    //     padding: '0 0 0 8px',
-    //     fontWeight: 600,
-    //     fontSize: '13px',
-    //     lineHeight: '18px',
-    //     color: '#444444',
-    //   },
-    //   '&:before': {
-    //     content: '""',
-    //     zIndex: 0,
-    //     position: 'absolute',
-    //     left: 0,
-    //     right: 0,
-    //     bottom: 0,
-    //     top: 0,
-    //     margin: 'auto 0',
-    //     width: '100%',
-    //     height: 1,
-    //     background: '#444444',
-    //   },
-    //
-    // output:
-    // position: relative;
-    // text-align: end;
-    // padding: 8px 0;
-    // margin: 0 16px;
-    // & span {
-    //     background: #fff;
-    //     position: relative;
-    //     z-index: 1;
-    //     padding: 0 0 0 8px;
-    //     font-weight: 600;
-    //     font-size: 13px;
-    //     line-height: 18px;
-    //     color: #444444;
-    // }
-    // &:before {
-    //     content: "";
-    //     z-index: 0;
-    //     position: absolute;
-    //     left: 0;
-    //     right: 0;
-    //     bottom: 0;
-    //     top: 0;
-    //     margin: auto 0;
-    //     width: 100%;
-    //     height: 1px;
-    //     background: #444444;
-    // }
-
 
     function camelToKebab(str) {
         return str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase();
@@ -83,22 +22,28 @@ document.addEventListener('DOMContentLoaded', function() {
             .replace(/,\s*$/g, '')
             .replace(/""""/g, '""')}
             }`
-        console.log(jsonInput)
+
         const parsedInput = JSON.parse(jsonInput);
         // Convert the JSON object to a CSS string
-        let cssString = '';
-        for (const [key, value] of Object.entries(parsedInput)) {
-            if (typeof value === 'object') {
-                cssString += `${key} {\n`;
-                for (const [subkey, subvalue] of Object.entries(value)) {
-                    cssString += `  ${camelToKebab(subkey)}: ${subvalue === '' ? '""' : subvalue};\n`;
+
+        const format = (obj, deep = 0) => {
+
+            return Object.entries(obj).reduce((acc, [key, value]) => {
+                const indent = new Array(deep).fill('  ').join('')
+                if (typeof value === 'object') {
+                    acc += `${indent}${key} {\n`;
+                    acc += format(value, deep + 1);
+                    acc += `${indent}}\n`;
+                } else {
+                    acc += `${indent}${camelToKebab(key)}: ${value === '' ? '""' : value};\n`;
                 }
-                cssString += '}\n';
-            } else {
-                cssString += `${camelToKebab(key)}: ${value === '' ? '""' : value};\n`;
+                return acc;
             }
+            , '');
         }
-        return cssString;
+
+
+        return format(parsedInput);
     }
 
 
